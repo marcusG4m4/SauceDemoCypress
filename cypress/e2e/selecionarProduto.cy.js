e7376cfb7d4c29188b99d8edf758c44162c4e561
Marcus Gama <sucramgama@gmail.com>
2025-11-07 09:54:10 -0300

    teste com massa

--- cypress/e2e/selecionarProduto.cy.js
+++ cypress/e2e/selecionarProduto.cy.js
@@ -2,6 +2,7 @@ import 'cypress-xpath'
 
 describe('selecionar produtos', () => {
   // atributos
+  const massa = require('../fixtures/massa') // importa a massa 
 
   beforeEach(() => {
     cy.visit('/')                             // abre o browser na url informada em cypress.config.js
@@ -29,7 +30,7 @@ describe('selecionar produtos', () => {
 
     cy.get('img[alt="Sauce Labs Backpack"]')
       .click()                                // clica na imagem do produto mochila 
- 
+
     // carregar a pagina de item de inventario  
 
     // apenas para demonstrar como fariamos com XPath Absoluto
@@ -45,17 +46,14 @@ describe('selecionar produtos', () => {
       .should('have.text', '$29.99')
 
     cy.get('#add-to-cart')
-        .click() // clica no botao adicionar carrinho 
+      .click() // clica no botao adicionar carrinho 
 
     cy.get('a.shopping_cart_link')
       .should('have.text', '1') // verifica se exibe o numero 1
-      .click()
-
-    //cy.get('a', 'shopping_cart_link')
-    //.click() // clica no botao carrinho 
+      .click()                  // clica para ir para o carrinho
 
     cy.get('span.title')
-      .should('have.text', 'Your Cart') // verificar o titulo da sessão
+      .should('have.text', 'Your Cart') // verificar o titulo da seção
 
     cy.get('div.inventory_item_name')
       .should('have.text', 'Sauce Labs Backpack') // verifica o titulo do produto
@@ -68,6 +66,67 @@ describe('selecionar produtos', () => {
 
   }) // termina o it
 
+  massa.array.forEach(({ username, productName, productPrice }) => {
+    it( `selecionar ${productName} - Usuario: ${username}`, () => {
+
+      cy.title()                                // verifica se o título da página é Swag Labs
+        .should('eq', 'Swag Labs')
+
+      // realizar login
+      cy.get('input[data-test="username"]')     // preencher o usuário
+        .type(username)
+
+      cy.get('#password')
+        .type('secret_sauce')                   // preencher a senha 
+
+      cy.get('input[name="login-button"]')
+        .click()                                // clica no botão login
+
+      // carregar a pagina de inventario
+
+      cy.get('span.title')
+        .should('have.text', 'Products')        // verifica se o elemento contém Products 
+
+      cy.get(`img[alt="${productName}"]`)
+        .click()                                // clica na imagem do produto mochila 
+
+      // carregar a pagina de item de inventario  
+
+      // apenas para demonstrar como fariamos com XPath Absoluto
+      // verifica se no elemento via Xpath contem o texto Back to products 
+      // Tem pelo menos 7 formas mais praticas e legiveis do que isso 
+      cy.xpath('/html/body/div/div/div/div[1]/div[2]/div/button')
+        .should('have.text', 'Back to products')
+
+      cy.get('div.inventory_details_name.large_size') // Verifica titulo
+        .should('have.text', productName)
+
+      cy.get('div.inventory_details_price')  // Verifica preço
+        .should('have.text', productPrice)
+
+      cy.get('#add-to-cart')
+        .click() // clica no botao adicionar no carrinho 
+
+      cy.get('a.shopping_cart_link')
+        .should('have.text', '1') // verifica se exibe o numero 1
+        .click()                  // clica para ir para o carrinho
+
+      cy.get('span.title')
+        .should('have.text', 'Your Cart') // verificar o titulo da sessão
+
+      cy.get('div.inventory_item_name')
+        .should('have.text', productName) // verifica o titulo do produto
+
+      cy.get('div.inventory_item_price')
+        .should('have.text', productPrice) // verifica o preço do produto
+
+      cy.get('div.cart_quantity')
+        .should('have.text', '1') // verificar quantidade de itens
+
+    }) // termina o it
+  }) // ternmina massa forEach
+
+  /* 
   afterEach(() => {
     cy.get('#remove-sauce-labs-backpack') // remove o produto do carrinho 
       .click()
@@ -80,8 +139,11 @@ describe('selecionar produtos', () => {
       //.should('not.be.disabled')      // garante que o botão está pronto pra clicar
       .click()
 
-      cy.get('#login-button')
+    cy.get('#login-button')
       .should('be.visible') // verificar se está novamente na tela de login
-  })
+
+  }) // termina o afterEach
+
+*/
 
 }) // termina describe'
